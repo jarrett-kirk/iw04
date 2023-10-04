2351f8bfb9c3840916f1ecb6736a83fa18853558
jaredkurt <jarrettkirk2002@gmail.com>
2023-10-04 09:50:57 -0400

    code

--- iw.py
+++ iw.py
@@ -16,8 +16,26 @@ app = flask.Flask(__name__, template_folder='pages')
 
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
-def index():
+def home():
 
     html_code = flask.render_template('home.html')
+    response = flask.make_response(html_code)
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/visualization', methods=['GET'])
+def visual():
+
+    html_code = flask.render_template('visualization.html')
+    response = flask.make_response(html_code)
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/coding', methods=['GET'])
+def coding():
+
+    html_code = flask.render_template('code.html')
     response = flask.make_response(html_code)
     return response
